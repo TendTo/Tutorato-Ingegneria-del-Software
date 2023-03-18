37e15cb60798c615bf03824b66f09c12a6d9e15f
Picred <andry4you@hotmail.it>
2023-03-18 17:29:26 +0100

    feat: InverseCalculator piu interattivo

--- lezioni/1-Java/esempi/InverseCalculator.java
+++ lezioni/1-Java/esempi/InverseCalculator.java
@@ -1,24 +1,37 @@
 import java.util.Random;
-import java.util.Scanner;
 
-// InverseCalculator.java
+// InverseCalculator.java update
 
 public class InverseCalculator {
-    private static char getRandomOp() {
-        Random random = new Random();
-        int op = random.nextInt(4);
+    private static int result;
 
-        switch (op) {
-            case 0:
-                return '+';
-            case 1:
-                return '-';
-            case 2:
-                return 'x';
-            case 3:
-                return '/';
-            default:
-                return 'e';
+
+    /**
+     * Genera un operatore casuale in base alla difficolta
+     * @param difficolta : livello di difficolta, 1 oppure 2
+     */
+    private static char getRandomOp(short difficolta) {
+        Random random = new Random();
+        int op = random.nextInt(2);
+        if(difficolta == 1){
+            switch (op) {
+                case 0:
+                    return '+';
+                case 1:
+                    return '-';
+                default:
+                    return 'e';
+            }
+        } 
+        else{
+            switch (op) {
+                case 0:
+                    return '*';
+                case 1:
+                    return '/';
+                default:
+                    return 'e';
+            }
         }
     }
 
@@ -28,39 +41,61 @@ public class InverseCalculator {
                 return a + b;
             case '-':
                 return a - b;
-            case 'x':
+            case '*':
                 return a * b;
             case '/':
-                return a / b;
+                    return a / b;
             default:
                 return 0;
         }
     }
 
-    private static int readAnswer() {
-        Scanner scanner = new Scanner(System.in);
-        int answer = scanner.nextInt();
-        scanner.close();
-        return answer;
+
+    /**Segnala l'errore ed esce dal programma */
+    private static void getError(){
+        System.err.println("Usage: java InverseCalculator <numeroDomande> <difficolta>");
+        System.err.print("<difficolta> puo' essere: 1, 2");
+        System.exit(1);
     }
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        int a = random.nextInt(20);
-        int b = random.nextInt(10);
 
-        char op = getRandomOp();
-        int result = calculateResult(a, b, op);
-
-        System.out.print("What is " + a + " " + op + " " + b + "? ");
-
-        // int answer = Integer.parseInt(System.console().readLine());
-        int answer = readAnswer();
-
-        if (answer == result) {
+    /**
+     * Verifica la correttezza della risposta data dall'utente
+     * @param answer : short
+    */
+    private static void checkAnswer(short answer){
+        if (answer == result)
             System.out.println("Correct!");
-        } else {
+        else
             System.out.println("Wrong!");
+    }
+
+
+    public static void main(String[] args) {
+        if(args.length != 2)
+            getError();
+
+        short numeroDomande = Short.parseShort(args[0]);
+        short difficolta = Short.parseShort(args[1]);
+
+        if(numeroDomande <= 0 || difficolta<1 || difficolta >2)
+            getError();
+
+
+        while(numeroDomande > 0){
+            Random random = new Random();
+            int a = random.nextInt(20);
+            int b = random.nextInt(10);
+            char op = getRandomOp(difficolta);
+            
+            InverseCalculator.result = calculateResult(a, b, op);
+
+            System.out.print("What is " + a + " " + op + " " + b + "? ");
+            short answer = Short.parseShort(System.console().readLine());
+
+            InverseCalculator.checkAnswer(answer);
+
+            numeroDomande--;
         }
     }
 }
