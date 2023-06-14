bd87dbce90019f3e54558f7ffa6d65bb0e4772fd
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-06-14 10:43:46 +0200

    fix(lezioni-9): change pizza to spaghetti

--- lezioni/9-Observer, Composite, Strategy, Command/esempi/command/SpaghettiCommand.java
+++ lezioni/9-Observer, Composite, Strategy, Command/esempi/command/SpaghettiCommand.java
@@ -3,8 +3,8 @@ import java.util.List;
 public class SpaghettiCommand extends PrepareMealCommand {
     @Override
     public void execute() {
-        Meal pizza = new Meal(List.of("pasta", "tomatoes", "cheese"), "spaghetti");
-        kitchen.cook(pizza);
-        kitchen.announceIsReady(pizza);
+        Meal spaghetti = new Meal(List.of("pasta", "tomatoes", "cheese"), "spaghetti");
+        kitchen.cook(spaghetti);
+        kitchen.announceIsReady(spaghetti);
     }
 }
